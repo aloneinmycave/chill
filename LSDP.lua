0b96f4a80caea7c4f24a0e385b7484922f6e0b50
aloneinmycave <89616175+aloneinmycave@users.noreply.github.com>
2021-08-26 22:15:10 -0500

    Create LSDP.lua

---
+++ LSDP.lua
@@ -0,0 +1,84 @@
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+	if msg == ".fly" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/fly.lua", true))()
+    elseif msg == ".korblox" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/korblox.lua", true))()
+	elseif msg == ".shazam" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/shazamfly.lua", true))()
+    elseif msg == ".noclip" then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/noclip.lua", true))()
+    elseif msg == ".buy rev" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buyrev.lua", true))()
+    elseif msg == ".rev ammo" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/revammo.lua", true))()
+    elseif msg == ".buy db" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buydb.lua", true))()
+    elseif msg == ".db ammo" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/dbammo.lua", true))()
+    elseif msg == ".buy bat" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buybat.lua", true))()
+    elseif msg == ".buy knife" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buyknife.lua", true))()
+    elseif msg == ".buy hammer" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buyhammer.lua", true))()
+    elseif msg == ".buy hamb" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buyhamb.lua", true))()
+    elseif msg == ".mask" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/mask.lua", true))()
+    elseif msg == ".buy rpg" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buyrpg.lua", true))()
+    elseif msg == ".rpg ammo" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/rpgammo.lua", true))()
+    elseif msg == ".buy gren" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buygren.lua", true))()
+    elseif msg == ".buy flash" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/buyflash.lua", true))()
+    elseif msg == ".casino" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/casino.lua", true))()
+    elseif msg == ".rev" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/rev.lua", true))()
+    elseif msg == ".db" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/db.lua", true))()
+    elseif msg == ".ufo" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/ufo.lua", true))()
+    elseif msg == ".admin1" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/admin1.lua", true))()
+    elseif msg == ".admin2" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/admin2.lua", true))()
+	elseif msg == ".animhack" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/animhack.lua", true))()
+	elseif msg == ".animhack2" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/animhack2.lua", true))()
+    elseif msg == ".lower" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/lowergfx.lua", true))()
+    elseif msg == ".pp" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/pp.lua", true))()
+	elseif msg == ".qtp" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/qtp.lua", true))()
+    elseif msg == ".katana" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/katana.lua", true))()
+    elseif msg == ".antiflash" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/antiflash.lua", true))()
+    elseif msg == ".headless" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/headless.lua", true))()
+    elseif msg == ".nuke" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/nuke.lua", true))()
+    elseif msg == ".holycross" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/holycross.lua", true))()
+    elseif msg == ".naked" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/naked.lua", true))()
+    elseif msg == ".tornado" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/tornado.lua", true))()
+    elseif msg == ".gbullet" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/gbullet.lua", true))()
+    elseif msg == ".bikefly" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/bikefly.lua", true))()
+    elseif msg == ".bgalaxy" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/aloneinmycave/primos-scripts/main/bgalaxy.lua", true))()
+
+            game:GetService("StarterGui"):SetCore("SendNotification",{
+			Title = "Credits";
+			Text = "Gracias a Scar.#0001";
+			Icon = "rbxassetid://5021827462";
+		})
+local A_1 = "[Primos-Script] Cargado correctamente." local A_2 = "All" local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2)
